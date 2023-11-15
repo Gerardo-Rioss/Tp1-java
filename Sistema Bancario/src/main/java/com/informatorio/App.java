4aebabe24467c54a8b573ea389c8c947e3b318dd
Gerardo-Rioss <gerarios26@gmail.com>
2023-11-15 00:24:22 -0300

    base

--- Sistema Bancario/src/main/java/com/informatorio/App.java
+++ Sistema Bancario/src/main/java/com/informatorio/App.java
@@ -1,13 +1,17 @@
 package com.informatorio;
 
+import com.informatorio.basededatos.BdClientes;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
+        
         System.out.println( "Hello World!" );
     }
 }
